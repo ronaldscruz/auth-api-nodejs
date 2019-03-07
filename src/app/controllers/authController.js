6487ceb07c427658243fca185b25ebfb38e48ff5
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-07 08:46:19 -0300

    recuperaçao de senha concluida

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
+const mailer = require('../modules/mailer')
 const authConfig = require('../../config/auth')
 
 const User = require('../models/User')
@@ -18,7 +20,7 @@ router.post("/register", async(req, res) => {
 
    try{
       if (await User.findOne({ email }))
-         return res.status(400).send({error: "ERROR: E-mail already exists on our database."})
+         return res.status(400).send("This e-mail already exists on our databases.")
 
       const newUser = await User.create(req.body)
 
@@ -29,7 +31,7 @@ router.post("/register", async(req, res) => {
          token: generateToken( { id: newUser.id } )
       })
    }catch(err){
-      return res.status(400).send({ error: "Registration failed." })
+      return res.status(400).send("Registration failed.")
    }
 })
 
@@ -39,10 +41,10 @@ router.post("/authenticate", async(req, res) => {
    const user = await User.findOne({ email }).select("+password")
 
    if (!user)
-      return res.status(400).send({ error: "Invalid username." })
+      return res.status(400).send("Invalid username.")
 
    if (!await bcrypt.compare(password, user.password))
-      return res.status(400).send({ error: "Invalid password." })
+      return res.status(400).send("Invalid password.")
 
    user.password = undefined
 
@@ -50,6 +52,74 @@ router.post("/authenticate", async(req, res) => {
       user, 
       token: generateToken( { id: user.id })
    })
+})
+
+router.post('/forgot_password', async(req, res) => {
+   const {email} = req.body
+
+   try{
+      const user = await User.findOne({email})
+
+      if (!user)
+         return res.status(400).send("Invalid username.")
+
+      const token = crypto.randomBytes(15).toString('hex')
+
+      const now = new Date()
+      now.setMinutes(now.getMinutes() + 15)
+
+      await User.findByIdAndUpdate(user.id, {
+         '$set': {
+            'passwordResetToken': token,
+            'passwordResetExpires': now
+         }
+      })
+
+      mailer.sendMail({
+         to: email,
+         from: 'ronald@fastcoding.com',
+         template: '/auth/forgot_password',
+         context: { token }
+      }, (err) => {
+         if(err)
+            return res.status(400).send('Failed to send recovery token! :(')
+         return res.send('Your token to redefine your password was sent!')
+      })
+
+      console.log(token, now)
+   }catch(err){
+      res.status(400).send("Password recovery failed")
+      console.log(err)
+   }
+})
+
+router.post('/reset_password', async(req, res) => {
+   const { email, token, password } = req.body
+
+   try{
+      const user = await User.findOne({email})
+         .select("+passwordResetToken passwordResetExpires")
+
+      if (!user)
+         return res.status(400).send("Invalid username.")
+
+      if (token !== user.passwordResetToken)
+         return res.status(400).send('Invalid token.')
+
+      const now = new Date()
+      if (now > user.passwordResetExpires)
+         return res.status(400).send('This token has expired. Generate a new one')
+
+      user.password = password
+      user.passwordResetToken = undefined
+      user.passwordResetExpires = undefined
+
+      await user.save()
+
+      return res.send("Password change successful! Now you can use your new credentials")
+   }catch(err){
+      res.status(400).send('Failed to reset password :(')
+   }
 })
 
 module.exports = app => app.use("/auth", router)
