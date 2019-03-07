e5fe0dbe7a03565928f53e2622355aaf9670475a
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-07 08:48:54 -0300

    modulo mailer adicionado

---
+++ src/app/modules/mailer.js
@@ -0,0 +1,25 @@
+require('dotenv').load()
+
+const path = require('path')
+const mailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
+
+const transport = mailer.createTransport({
+   host: process.env.MAILER_HOST,
+   port: process.env.MAILER_PORT,
+   auth: {
+     user: process.env.MAILER_USER,
+     pass: process.env.MAILER_PASS,
+   }
+});
+
+transport.use('compile', hbs({
+   viewEngine: {
+      extname: '.hbs',
+      partialsDir: path.resolve('../resources/mail/partials')
+   },
+   viewPath: path.resolve('./src/resources/mail'),
+   extName: '.hbs'
+}))
+
+module.exports = transport
