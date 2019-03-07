c1e46b5c91842917839b305c96572300dfc11b38
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-07 02:45:46 -0300

    utilizando variaveis de ambiente

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').load()
+
 const express = require('express')
 const cors = require("cors")
 const bodyParser = require('body-parser')
@@ -8,9 +10,10 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-require('./controllers/authController')(app)
+require('./app/controllers/authController')(app)
+require('./app/controllers/panelController')(app)
 
-app.listen(3003)
+app.listen(process.env.PORT)
 
 
 
