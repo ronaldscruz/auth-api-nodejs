cefe342e2eda59e5fd1370ce83e7a8559d927d7e
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-08 10:46:44 -0300

    modelo de noticias adicionado

---
+++ src/app/models/News.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+const NewsSchema = new mongoose.Schema({
+   category: {
+      type: String,
+      required: true
+   },
+
+   title: {
+      type: String,
+      required: true,
+      min: [6, 'Too short title'],
+      max: [90, 'Too long title']
+   },
+
+   lead: {
+      type: String,
+      required: true,
+      min: [6, 'Too short lead'],
+      max: [450, 'Too long lead']
+   },
+
+   body: {
+      type: String,
+      required: true,
+      min: [10, 'Too short body']
+   },
+
+   author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+   },
+
+   location: {
+      type: String,
+      required: [true, 'Please, enter the location where the event occurred']
+   },
+
+   date: {
+      type: Date,
+      required: true,
+      default: Date.now()
+   },
+})
+
+const News = mongoose.model("News", NewsSchema)
+
+module.exports = News
