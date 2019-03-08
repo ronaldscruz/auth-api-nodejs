e10a99d9f880203f8e0d75a3bdff2f3011d68563
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-08 10:46:26 -0300

    crud criado

---
+++ src/app/controllers/newsController.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const router = express.Router()
+const authMiddlware = require('../middlewares/auth')
+
+const User = require('../models/User')
+const News = require('../models/News')
+
+router.use(authMiddlware)
+
+
+// List 
+router.get('/', async(req, res) => {
+   try{
+      const news = await News.find().populate('author')
+      return res.send(news)
+   }catch(err){
+      return res.status(400).send({error: "Error loading your news"})
+   }
+})
+
+
+// Read
+router.get('/:id', async(req, res) => {
+   try{
+      const news = await News.findById(req.params.id).populate('author')
+      return res.send(news)
+   }catch(err){
+      return res.status(400).send({error: "Error reading your news"})
+   }
+})
+
+
+// Create
+router.post('/create', async(req, res) => {
+   try{
+      const news = await News.create({...req.body, author: req.userId})
+      return res.send(news)
+   }catch(err){
+      return res.status(400).send({error: "Error creating your news"})
+   }
+})
+
+
+// Update
+router.put('/update/:id', async(req, res) => {
+   try{
+      const news = await News.findByIdAndUpdate(req.params.id, {...req.body, author: req.userId})
+      return res.send(news)
+   }catch(err){
+      return res.status(400).send({error: "Error updating your news"})
+   }
+})
+
+
+// Delete
+router.delete('/remove/:id', async(req, res) => {
+   try{
+      const news = await News.findByIdAndRemove(req.params.id)
+      return res.send(news)
+   }catch(err){
+      return res.status(400).send({error: "Error removing your news"})
+   }
+})
+
+
+module.exports = app => app.use("/manage_news", router)
