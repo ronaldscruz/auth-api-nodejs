7b2a6045c55267c5ac4c2e731ee0dabfc83e2b32
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-14 18:31:53 -0300

    conexao banco - newurlparser

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_CONNECT)
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
