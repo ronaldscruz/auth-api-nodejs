1185825dfb895538a2ea5c3b319694f9bd6641e5
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-14 18:20:33 -0300

    authcontroller corrections

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -83,7 +83,7 @@ router.post('/forgot_password', async(req, res) => {
       }, (err) => {
          if(err)
             return res.status(400).send({error: 'Failed to send recovery token! :('})
-         return res.send({error: 'Your token to redefine your password was sent!'})
+         return res.send({ok: 'Your token to redefine your password was sent!'})
       })
 
       console.log(token, now)
