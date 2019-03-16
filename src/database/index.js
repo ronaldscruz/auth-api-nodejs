f018f13ebfc5f572f5db3f173e536b7d45bd777d
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-16 14:26:00 -0300

    mongoclient removido

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true, useMongoClient: true })
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true})
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
