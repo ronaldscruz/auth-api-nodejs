9cf857511cb83fc207ac8767e1e59c5f9da20ded
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-16 16:03:57 -0300

    conexao ao banco em bloco try

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true})
-mongoose.Promise = global.Promise
+try{
+   mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true})
+   mongoose.Promise = global.Promise
+}catch(err){
+   console.log(err)
+}
 
 module.exports = mongoose
