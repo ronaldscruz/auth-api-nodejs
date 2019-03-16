3bf30703e5d6a157ed09a4deb4e85e1b602334e0
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-16 13:42:48 -0300

    database utilizando promise

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.Promise = global.Promise
 
 module.exports = mongoose
