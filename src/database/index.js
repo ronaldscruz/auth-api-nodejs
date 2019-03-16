70ffd256a9275ec6c78a3e0d92eb444184c03297
ronaldscruz <ronald.scruz16@gmail.com>
2019-03-16 12:58:10 -0300

    conexao atualizada / nao precisa mais de promise

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true })
-mongoose.Promise = global.Promise
 
 module.exports = mongoose
